76ec71277378d0bfdd67072e573c4ec0d46cffd0
đức hoàng <nghoangducit@gmail.com>
2020-10-30 08:59:11 +0700

    add redux

---
+++ src/redux/reducer/rootReducer.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux'
+import {ProductReducer} from './productReducer'
+
+const rootReducer = combineReducers({
+    product:ProductReducer
+});
+export default rootReducer;
